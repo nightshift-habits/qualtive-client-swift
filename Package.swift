9063a322d9c6507c24230f01c8d6c134d4688fed
Gustaf Rosenblad <gustaf@ccode.se>
2026-08-26 09:57:31 +0200

    Raise minimum platforms to iOS 15, macOS 12, tvOS 15, and watchOS 8.

--- Package.swift
+++ Package.swift
@@ -5,10 +5,10 @@ import PackageDescription
 let package = Package(
   name: "Qualtive",
   platforms: [
-    .macOS(.v10_13),
-    .iOS(.v12),
-    .tvOS(.v12),
-    .watchOS(.v4),
+    .macOS(.v12),
+    .iOS(.v15),
+    .tvOS(.v15),
+    .watchOS(.v8),
   ],
   products: [
     .library(
